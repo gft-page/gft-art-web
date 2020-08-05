d17da488f151b719e9c1fcbab895064eef0f3a83
Austin Griffith <austin@concurrence.io>
2020-08-05 12:16:41 -0600

    show how it is broken

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react";
 import "antd/dist/antd.css";
-import { getDefaultProvider, JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
+import { getDefaultProvider, JsonRpcProvider, Web3Provider, InfuraProvider } from "@ethersproject/providers";
 import "./App.css";
-import { Row, Col } from "antd";
+import { Row, Col, Button } from "antd";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { useUserAddress, useBalance } from "eth-hooks";
@@ -10,6 +10,9 @@ import { useExchangePrice, useGasPrice, useUserProvider } from "./hooks";
 import { Header, Account, Faucet, Ramp, Contract } from "./components";
 import Hints from "./Hints";
 import { INFURA_ID } from "./constants";
+
+const { ethers } = require("ethers");
+const BurnerProvider = require("burner-provider");
 
 const web3Modal = new Web3Modal({
   // network: "mainnet", // optional
@@ -25,10 +28,8 @@ const web3Modal = new Web3Modal({
 });
 
 // 🛰 providers
-const mainnetProvider = getDefaultProvider("mainnet", { infura: INFURA_ID });
-const localChainProvider = new JsonRpcProvider(
-  process.env.REACT_APP_PROVIDER ? process.env.REACT_APP_PROVIDER : "http://localhost:8545",
-);
+const mainnetProvider = new InfuraProvider("mainnet", { infura: INFURA_ID });
+const localChainProvider = mainnetProvider
 
 const logoutOfWeb3Modal = async () => {
   await web3Modal.clearCachedProvider();
@@ -39,16 +40,16 @@ const logoutOfWeb3Modal = async () => {
 };
 
 function App() {
-  const [injectedProvider, setInjectedProvider] = useState();
-  const price = useExchangePrice(mainnetProvider);
+  //const [injectedProvider, setInjectedProvider] = useState();
+  //const price = useExchangePrice(mainnetProvider);
   // const gasPrice = useGasPrice("fast");
 
   // Use your injected provider from 🦊 Metamask or if you don't have it then instantly generate a 🔥 burner wallet.
-  const userProvider = useUserProvider(injectedProvider, localChainProvider);
-  const address = useUserAddress(userProvider);
+  //const userProvider = useUserProvider(injectedProvider, localChainProvider);
+  //const address = useUserAddress(userProvider);
 
   // 🏗 scaffold-eth is full of handy hooks like this one to get your balance:
-  const yourLocalBalance = useBalance(localChainProvider, address);
+  //const yourLocalBalance = useBalance(localChainProvider, address);
   // just plug in different 🛰 providers to get your balance on different chains:
   // const yourMainnetBalance = useBalance(mainnetProvider, address);
 
@@ -57,7 +58,7 @@ function App() {
   // console.log("readContracts",readContracts)
   // const owner = useCustomContractReader(readContracts?readContracts['YourContract']:"", "owner")
 
-  const loadWeb3Modal = useCallback(async () => {
+  /*const loadWeb3Modal = useCallback(async () => {
     const provider = await web3Modal.connect();
     setInjectedProvider(new Web3Provider(provider));
   }, [setInjectedProvider]);
@@ -66,13 +67,42 @@ function App() {
     if (web3Modal.cachedProvider) {
       loadWeb3Modal();
     }
-  }, [loadWeb3Modal]);
+  }, [loadWeb3Modal]);*/
 
   return (
     <div className="App">
       <Header />
 
-      <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
+      <Button onClick={async ()=>{
+        console.log("🚀 TESTING with https://mainnet.infura.io:")
+        let burner = new BurnerProvider("https://mainnet.infura.io/v3/9ba908922edc44d1b5e1f0ba4506948d")
+        console.log("🔥📡 burner",burner)
+        let ethersProvider = new ethers.providers.Web3Provider(burner)
+        console.log("💩 provider:",ethersProvider)//.connect()
+        let accounts = await ethersProvider.listAccounts()
+        console.log("😅 accounts:",accounts)
+        setTimeout(async ()=>{
+          let bal = await ethersProvider.getBalance(accounts[0])
+          console.log("💵 balance", bal)
+        },3000)
+      }}>Mainnet Test</Button>
+
+      <Button onClick={async ()=>{
+        console.log("🚀 TESTING with https://dai.poa.network:")
+        let burner = new BurnerProvider("https://dai.poa.network")
+        console.log("🔥📡 burner",burner)
+        console.log("🔬🦠 blocktracker",burner._blockTracker)
+        let ethersProvider = new ethers.providers.Web3Provider(burner)
+        console.log("💩 provider:",ethersProvider)//.connect()
+        let accounts = await ethersProvider.listAccounts()
+        console.log("😅 accounts:",accounts)
+        setTimeout(async ()=>{
+          let bal = await ethersProvider.getBalance(accounts[0])
+          console.log("💵 balance", bal)
+        },3000)
+      }}>xDAI Test</Button>
+
+      {/*<div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
         <Account
           address={address}
           localProvider={localChainProvider}
@@ -84,27 +114,10 @@ function App() {
           logoutOfWeb3Modal={logoutOfWeb3Modal}
         />
       </div>
+*/}
 
-      {/*
-          🎛 this scaffolding is full of commonly used components
-          this <Contract/> component will automatically parse your ABI
-          and give you a form to interact with it locally
-      */}
 
-      <Contract name="YourContract" provider={userProvider} address={address} />
 
-      <Hints address={address} yourLocalBalance={yourLocalBalance} price={price} mainnetProvider={mainnetProvider} />
-
-      <div style={{ position: "fixed", textAlign: "left", left: 0, bottom: 20, padding: 10 }}>
-        <Row align="middle" gutter={4}>
-          <Col span={9}>
-            <Ramp price={price} address={address} />
-          </Col>
-          <Col span={15}>
-            <Faucet localProvider={localChainProvider} price={price} />
-          </Col>
-        </Row>
-      </div>
     </div>
   );
 }
