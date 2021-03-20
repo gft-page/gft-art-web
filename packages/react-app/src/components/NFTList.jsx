6627d446e7844f4e87f1a7d7c177acfe9b7fdb53
valmack <8445610+valmack@users.noreply.github.com>
2021-03-20 16:49:43 -0400

    feat(recv): add burner keys to nft

--- packages/react-app/src/components/NFTList.jsx
+++ packages/react-app/src/components/NFTList.jsx
@@ -5,7 +5,11 @@ import NFT from './NFT';
 export default function NFTList(props) {
   const nftList = props.list?.map((nft) => {
     return (
-      <NFT address={nft.tokenAddress} id={nft.tokenId} network={props.network} />
+      <div key={nft.createdDate}>
+        <NFT address={nft.tokenAddress} id={nft.tokenId} network={props.network} />
+        <div>Burner address: {nft.burnerAddress}</div>
+        <div>Burner private key: {nft.burnerKey}</div>
+      </div>
     );
   });
   return (
