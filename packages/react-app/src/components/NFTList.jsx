4f638479d2955d883a6fd0b68c8b52fcae6d8221
Val <8445610+valmack@users.noreply.github.com>
2021-03-20 16:02:12 -0400

    Receiver nfts (#4)
    * feat(receiver): show nft list on login
    
    * fix(recv): handle no nfts in list

---
+++ packages/react-app/src/components/NFTList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NFT from './NFT';
+
+
+export default function NFTList(props) {
+  const nftList = props.list?.map((nft) => {
+    return (
+      <NFT address={nft.tokenAddress} id={nft.tokenId} network={props.network} />
+    );
+  });
+  return (
+    <div>
+      {nftList}
+    </div>
+  );
+}
