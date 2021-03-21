6eaea9c69df0b20bcf412d69b6b7d7ae40169b6b
valmack <8445610+valmack@users.noreply.github.com>
2021-03-21 07:15:46 -0400

    feat: style header

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -12,7 +12,7 @@ import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container';
 
 import "antd/dist/antd.css";
-import { Row, Col } from "antd";
+import { Row, Col, Image, PageHeader } from "antd";
 
 import { useUserAddress } from "eth-hooks";
 import { Header, Account, Faucet, Ramp, Contract, GasGauge, ThemeSwitch } from "./components";
@@ -21,6 +21,10 @@ import { JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { INFURA_ID, DAI_ADDRESS, DAI_ABI, NETWORK, NETWORKS } from "./constants";
+
+import logo from './logo.svg';
+
+const { Content } = PageHeader;
 
 const mainnetInfura = new JsonRpcProvider("https://mainnet.infura.io/v3/" + INFURA_ID)
 
@@ -64,35 +68,50 @@ function App() {
     return (
         <div className="App">
             <ThemeSwitch />
-            <div style={{ textAlign: "right", right: 0, top: 0, padding: 10, position: 'abosolute' }}>
-                <Account
-                    address={address}
-                    network={network}
-                    // localProvider={localProvider}
-                    userProvider={userProvider}
-                    mainnetProvider={mainnetProvider}
-                    // price={price}
-                    web3Modal={web3Modal}
-                    loadWeb3Modal={loadWeb3Modal}
-                    logoutOfWeb3Modal={logoutOfWeb3Modal}
-                    blockExplorer={blockExplorer}
-                />
-            </div>
-
             <Container className="col-md-10">
                 <Router>
-                    <Row gutter={16}>
-                        <Col>                   
-                        <NavLink exact to="/" activeClassName="selected">Send</NavLink>
-                        </Col>
-                        <Col>
-                        <NavLink to="/redeem" activeClassName="selected">Redeem</NavLink>
-                        </Col>
-                    </Row>
-                        <Container>
-                            <Route exact path="/" component={() => <SendersContainer web3Modal={web3Modal} network={network} />} />
-                            <Route path="/redeem" component={() => <ReceiversContainer web3Modal={web3Modal} network={network} />} />
-                        </Container>
+<Container>
+                    <PageHeader
+                        title={
+                            <Image
+                                width={200}
+                                src={logo}
+                            />
+                        }
+                        className="site-page-header"
+                        subTitle={
+                            <Row gutter={16}>
+                                <Col>
+                                    <NavLink exact to="/" activeClassName="selected">Send</NavLink>
+                                </Col>
+                                <Col>
+                                    <NavLink to="/redeem" activeClassName="selected">Redeem</NavLink>
+                                </Col>
+                            </Row>
+                        }
+                        extra={[
+                            <Account
+                                address={address}
+                                network={network}
+                                // localProvider={localProvider}
+                                userProvider={userProvider}
+                                mainnetProvider={mainnetProvider}
+                                // price={price}
+                                web3Modal={web3Modal}
+                                loadWeb3Modal={loadWeb3Modal}
+                                logoutOfWeb3Modal={logoutOfWeb3Modal}
+                                blockExplorer={blockExplorer}
+                            />
+                        ]}
+                    >
+                    </PageHeader>
+
+                    </Container>
+
+                    <Container>
+                        <Route exact path="/" component={() => <SendersContainer web3Modal={web3Modal} network={network} />} />
+                        <Route path="/redeem" component={() => <ReceiversContainer web3Modal={web3Modal} network={network} />} />
+                    </Container>
                 </Router>
             </Container>
         </div>
