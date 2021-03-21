faae2f5a9d3906191a3a9040c81c3d3541845d12
valmack <8445610+valmack@users.noreply.github.com>
2021-03-21 06:54:19 -0400

    feat: move jumbotron inside containers

--- packages/react-app/src/containers/ReceiversContainer.js
+++ packages/react-app/src/containers/ReceiversContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 //import {fetchAccounts} from '../actions/fetchAccounts'
 import Receivers from '../components/Receivers'
 import RedeemGuide from '../components/RedeemGuide'
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 class ReceiversContainer extends React.Component {
 
@@ -13,8 +14,10 @@ class ReceiversContainer extends React.Component {
     render() {
         return (
             <div>
+                    <Jumbotron className="bg-light shadow-sm">
                 <RedeemGuide />
                 <Receivers web3Modal={this.props.web3Modal} />
+                </Jumbotron>
             </div>
         )
     }
