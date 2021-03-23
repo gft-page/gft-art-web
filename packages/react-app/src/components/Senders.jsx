fcebf3fecc074e659a00dd8694f59599dad5dc3a
valmack <8445610+valmack@users.noreply.github.com>
2021-03-22 23:15:27 -0400

    feat(send): move select and add loading

--- packages/react-app/src/components/Senders.jsx
+++ packages/react-app/src/components/Senders.jsx
@@ -34,13 +34,13 @@ class Senders extends React.Component {
       twitterUsers: [],
       twitterMap: {},
       customMarketplace: '',
+      marketplaceCheckPending: false,
       marketplaceAlert: '',
       isApproved: true,
       value: 0,
       provider: null,
       selectIsOpen: false
     }
-
   }
 
   componentDidMount() {
@@ -49,11 +49,9 @@ class Senders extends React.Component {
     });
   }
 
-
   using721() {
     return this.state.marketplace && this.state.marketplace.includes('721') || this.state.marketplace == "ZORA"
   }
-
 
   getTweets(ID) {
     fetch(`https://api.gft.art/api/v1/twitter/${ID}/replies?limit=50`)
@@ -62,8 +60,6 @@ class Senders extends React.Component {
 
     return "@NFTgirl My first NFT sale was my genesis piece on @KnownOrigin_io picked up my the OG himself @j1mmyeth - it's the intro to my animation reel and a personal favorite of mine. Also happened the same day Biden was projected to win the election. So it was a very good day \uD83D\uDE42"
   }
-
-
 
   processReplies(json) {
     console.log(json.usernames)
@@ -118,42 +114,28 @@ class Senders extends React.Component {
       this.setState({addresses: addresses})
   }
 
-
-
-
   handleSelectChange = event => {
     this.setState({
       selectIsOpen: false,
-      marketplace: event
+      marketplace: event,
+      selectIsLoading: true
     }, () => {
-      setTimeout(() => {
       if (!this.state.marketplace.includes('CUSTOM')) {
         this.handleMarketplaceCheck(event);
       }
-    }, 100);
     });
   }
 
   handleCustomMarketplaceChange = event => {
-
     if (this.state.marketplace.includes('CUSTOM') && this.state.customMarketplace.length > 0) {
-      this.handleMarketplaceCheck(event)
+      this.handleMarketplaceCheck(event);
     }
-
   }
 
-
   handleTwitterUsersValueChange = event => {
-
-
-
     const old =  this.state.twitterMap ? {... this.state.twitterMap} : {}
-  old[(event.target.id.split("_")[1]).toLowerCase()] = event.target.value
-    
-
-  this.setState({twitterMap: old})
-
-
+    old[(event.target.id.split("_")[1]).toLowerCase()] = event.target.value
+    this.setState({twitterMap: old})
   }
 
   handleCheckChange = event => {
@@ -175,29 +157,31 @@ class Senders extends React.Component {
   }
 
   handleMarketplaceCheck = async (event) => {
-    let nftContract = ''
-    console.log(this.state.marketplace.includes("CUSTOM"))
+    this.setState({ marketplaceCheckPending: true });
+
+    let nftContract = '';
+    console.log(this.state.marketplace.includes("CUSTOM"));
     if (!this.state.marketplace.includes("CUSTOM")) {
-      nftContract = this.state.marketplace
+      nftContract = this.state.marketplace;
     } else {
-      nftContract = this.state.customMarketplace
+      nftContract = this.state.customMarketplace;
     }
 
-    console.log(`handleMarketplaceCheck nftContract: ${nftContract}`)
+    console.log(`handleMarketplaceCheck nftContract: ${nftContract}`);
+
     try {
-    let isApproved = await checkApproved(this.state.provider, nftContract)
-    console.log(isApproved)
-    this.setState({ isApproved: isApproved.approved })
-
-    if (!isApproved.approved && isApproved.error) 
-      this.setState({    marketplaceAlert: String(isApproved.error)   })
-      else 
-       this.setState({marketplaceAlert: ''})
-
+      let isApproved = await checkApproved(this.state.provider, nftContract);
+      console.log(isApproved);
+      this.setState({ isApproved: isApproved.approved });
+      if (!isApproved.approved && isApproved.error) {
+        this.setState({ marketplaceAlert: String(isApproved.error) });
+      } else {
+      this.setState({marketplaceAlert: ''});
+      }
     } catch (err) {
       message.error('Please connect your wallet first');
-      return;
     }
+    this.setState({ marketplaceCheckPending: false });
   }
 
   handleMarketplaceSubmit = async (event) => {
@@ -279,6 +263,52 @@ class Senders extends React.Component {
       <div>
         <h1><small><strong>Send multiple NFTs to many, all at once ✨</strong></small></h1>
         <Jumbotron>
+          <p>
+          <strong>Select the protocol or marketplace your NFT is listed on</strong>
+          </p>
+              <Form
+                onSubmit={event => this.handleMarketplaceSubmit(event)}
+                name="marketplace"
+                layout="inline"
+              >
+                <Form.Item
+                  name="select"
+                  label=""
+                >
+                  <Select
+                    value={this.state.marketplace}
+                    onClick={this.handleSelectClick}
+                    onChange={this.handleSelectChange}
+                    placeholder="Please select a marketplace"
+                    open={this.state.selectIsOpen}
+                    loading={this.state.marketplaceCheckPending}
+                    style={{ width: "200px" }}
+                  >
+                    <option value="ZORA">Zora</option>
+                    <option value="RARIBLE_721">Rarible 721</option>
+                    <option value="RARIBLE_1155">Rarible 1155</option>
+                    <option value="CUSTOM_721">Custom 721 Address</option>
+                    <option value="CUSTOM_1155">Custom 1155 Address</option>
+                  </Select>
+                </Form.Item>
+                {this.state.marketplace.includes("CUSTOM") ?
+                  <Form.Item
+                    onChange={this.handleChange} value={this.state.customMarketplace}
+                    label=""
+                    name="customMarketplace"
+                  >
+                    <Input name="customMarketplace" placeholder="Enter custom NFT contract address" style={{ minWidth: "350px" }} onChange={this.handleCustomMarketplaceChange} onKeyPress={this.handleCustomMarketplaceChange} onClick={this.handleCustomMarketplaceChange} onBlur={this.handleCustomMarketplaceChange}/>
+                  </Form.Item>
+                  : null}
+                {!this.state.isApproved ?
+                  <>
+                    <Button type="primary" onClick={event => this.handleMarketplaceSubmit(event)}>Approve NFT Transfer</Button>
+                    <br></br>
+                    {this.state.marketplaceAlert}</>
+                  : null}
+              </Form>
+              <br />
+              <br />
         <Row gutter={16}>
           <Col className="gutter-row" span={11}>
             <div>
@@ -320,49 +350,6 @@ class Senders extends React.Component {
           </Col>
           <Col className="gutter-row" span={13}>
             <div>
-              <strong>Select the protocol or marketplace your NFT is listed on</strong>
-              <Form
-                onSubmit={event => this.handleMarketplaceSubmit(event)}
-                name="marketplace"
-              >
-                <Form.Item
-                  name="select"
-                  label=""
-                >
-                  <Select
-                    value={this.state.marketplace}
-                    onClick={this.handleSelectClick}
-                    onChange={this.handleSelectChange}
-                    // onSelect={() => {this.setState({ selectIsOpen: false })}}
-                    placeholder="Please select a marketplace"
-                    open={this.state.selectIsOpen}
-                  >
-                    <option value="ZORA">Zora</option>
-                    <option value="RARIBLE_721">Rarible 721</option>
-                    <option value="RARIBLE_1155">Rarible 1155</option>
-                    <option value="CUSTOM_721">Custom 721 Address</option>
-                    <option value="CUSTOM_1155">Custom 1155 Address</option>
-                  </Select>
-                </Form.Item>
-                {this.state.marketplace.includes("CUSTOM") ?
-                  <Form.Item
-                    onChange={this.handleChange} value={this.state.customMarketplace}
-                    label=""
-                    name="customMarketplace"
-                  >
-                    <Input name="customMarketplace" placeholder="Enter custom NFT contract address" style={{ width: '50%' }} onChange={this.handleCustomMarketplaceChange} onKeyPress={this.handleCustomMarketplaceChange} onClick={this.handleCustomMarketplaceChange} onBlur={this.handleCustomMarketplaceChange}/>
-                  </Form.Item>
-                  : null}
-                {!this.state.isApproved ?
-                  <>
-                    <Button type="primary" onClick={event => this.handleMarketplaceSubmit(event)}>Approve NFT Transfer</Button>
-                    <br></br>
-                    {this.state.marketplaceAlert}</>
-                  : null}
-
-
-              </Form>
-              <br></br>
               <Form
                 onSubmit={event => this.handleGiftSubmit(event)}
                 name="gift"
